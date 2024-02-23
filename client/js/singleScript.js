30a76246e5dfbfba8f9190ff296a8ffd86231267
maddiemaz <maddiemaz18@gmail.com>
2024-02-23 14:05:06 -0500

    another attempt to update Github

--- client/js/singleScript.js
+++ client/js/singleScript.js
@@ -9,6 +9,10 @@ const getData = async () => {
     const responseCards = await axios.get(`http://localhost:3001/cards`)
     console.log(responseCards)
 
+    const responseMeanings = await axios.get(`http://localhost:3001/meanings`)
+    console.log(responseMeanings)
+
+    // Card Content
     const card_Suit = document.querySelector('#suitName')
     card_Suit.innerText = responseCards.data[8].suit
     const card_Name = document.querySelector('#headerName')
@@ -40,6 +44,44 @@ const getData = async () => {
     const card_meaningRev = document.querySelector('#meaning-Rev')
     card_meaningRev.innerText = responseCards.data[8].rev_gen
 
+    // Additional Meanings
+    const mmCareer_reader = document.querySelector('#mystic-Career-reader')
+    mmCareer_reader.innerText = responseMeanings.data[0].reader
+    const mmCareer_intention = document.querySelector('#mystic-Career-intention')
+    mmCareer_intention.innerText = responseMeanings.data[0].intention
+    const mmCareer_up = document.querySelector('#mystic-CareerUp-content')
+    mmCareer_up.innerText = responseMeanings.data[0].up_gen
+    const mmCareer_rev = document.querySelector('#mystic-CareerRev-content')
+    mmCareer_rev.innerText = responseMeanings.data[0].rev_gen
+
+    const mmFinances_reader = document.querySelector('#mystic-Finances-reader')
+    mmFinances_reader.innerText = responseMeanings.data[1].reader
+    const mmFinances_intention = document.querySelector('#mystic-Finances-intention')
+    mmFinances_intention.innerText = responseMeanings.data[1].intention
+    const mmFinances_up = document.querySelector('#mystic-FinancesUp-content')
+    mmFinances_up.innerText = responseMeanings.data[1].up_gen
+    const mmFinances_rev = document.querySelector('#mystic-FinancesRev-content')
+    mmFinances_rev.innerText = responseMeanings.data[1].rev_gen
+
+    const mmLove_reader = document.querySelector('#mystic-Love-reader')
+    mmLove_reader.innerText = responseMeanings.data[2].reader
+    const mmLove_intention = document.querySelector('#mystic-Love-intention')
+    mmLove_intention.innerText = responseMeanings.data[2].intention
+    const mmLove_up = document.querySelector('#mystic-LoveUp-content')
+    mmLove_up.innerText = responseMeanings.data[2].up_gen
+    const mmLove_rev = document.querySelector('#mystic-LoveRev-content')
+    mmLove_rev.innerText = responseMeanings.data[2].rev_gen
+    const mmLove_note = document.querySelector('#mystic-LoveNote-content')
+    mmLove_note.innerText = responseMeanings.data[2].note
+
+    const dDecision_reader = document.querySelector('#daily-Decision-reader')
+    dDecision_reader.innerText = responseMeanings.data[3].reader
+    const dDecision_intention = document.querySelector('#daily-Decision-intention')
+    dDecision_intention.innerText = responseMeanings.data[3].intention
+    const dDecision_layout = document.querySelector('#daily-Decision-title')
+    dDecision_layout.innerText = `Should you say ${responseMeanings.data[3].layout}?`
+    const dDecision_up = document.querySelector('#daily-Decision-content')
+   dDecision_up.innerText = responseMeanings.data[3].up_gen
     
 }
 getData()
